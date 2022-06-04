3351e825dbc5f9153d04616d6d71f8699eced079
Hitesh Songra <hgsongra@ymail.com>
2022-06-04 17:07:08 +0530

    Seed admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create(email: 'user1' ,password: 123456)
-User.create(email: 'user2',password: 123456)
+User.create!(email: 'admin@example.com', password: 'Admin@1234')
