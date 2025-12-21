7f5a66621a9fe2d8c104092dbd88ebe4975bca9c
Hitesh Songra <hgsongra@ymail.com>
2025-12-21 16:39:38 +0530

    Added Date validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,15 @@ class Customer < ApplicationRecord
     validates :contact, length: { maximum: 10 }, numericality: { only_integer: true }
     validates :pant_lambai, :pant_mori, :pant_kamar, :pant_seat, :pant_jang, :pant_joro,  presence: true
     validates :shirt_kolar, :shirt_lambai, :shirt_shoulder, :shirt_baay, :shirt_chhati, :shirt_kamar, :shirt_seat, presence: true
+    validate :delivery_date_must_be_in_future, if: -> { delivery_date.present? }
     
     paginates_per 10
+
+    private
+
+    def delivery_date_must_be_in_future
+        if delivery_date < Date.current
+            errors.add(:delivery_date, "ભવિષ્યની તારીખ હોવી જોઈએ")
+        end
+    end
 end
