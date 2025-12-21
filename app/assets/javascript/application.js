cae31beff2ea07eb5ee6493f86db3ab1ca0c6fe6
Hitesh Songra <hgsongra@ymail.com>
2025-12-21 17:59:59 +0530

    Fix hamburger menu

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,4 +1,16 @@
 //= require jquery3
-//= require jquery_ujs
 //= require popper
-//= require bootstrap-sprockets
+//= require bootstrap
+
+// Reinitialize Bootstrap components after Turbo page loads
+document.addEventListener('turbo:load', function() {
+  // Dispose and reinitialize navbar collapse to prevent duplicate listeners
+  var navbarCollapse = document.getElementById('navbarNav');
+  if (navbarCollapse) {
+    var existingCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
+    if (existingCollapse) {
+      existingCollapse.dispose();
+    }
+    new bootstrap.Collapse(navbarCollapse, { toggle: false });
+  }
+});
