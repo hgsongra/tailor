73d3e27fc05dfd20fd5841f8a786afa6df562260
Hitesh Songra <hgsongra@ymail.com>
2025-12-21 15:37:07 +0530

    Added contact validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
     validates :name, :city, :contact, presence: true
+    validates :contact, length: { maximum: 10 }, numericality: { only_integer: true }
     validates :pant_lambai, :pant_mori, :pant_kamar, :pant_seat, :pant_jang, :pant_joro,  presence: true
     validates :shirt_kolar, :shirt_lambai, :shirt_shoulder, :shirt_baay, :shirt_chhati, :shirt_kamar, :shirt_seat, presence: true
     
