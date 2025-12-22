c73302fd8aa13ddbc7d05c72ecefbf4d14b2be16
Hitesh Songra <hgsongra@ymail.com>
2025-12-22 15:31:27 +0530

    Updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_17_164611) do
+ActiveRecord::Schema[7.0].define(version: 2025_12_22_092534) do
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "city"
@@ -32,6 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_17_164611) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "notes"
+    t.string "customer_no"
+    t.decimal "shirt_kaf"
   end
 
   create_table "users", force: :cascade do |t|
