01cfdd599adeadf4272e9a4e2f70e6634d4c598a
Hitesh Songra <hgsongra@ymail.com>
2025-12-30 17:49:04 +0530

    Added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :customers
+
+  # Database backup/restore (requires authentication)
+  get "backup", to: "backups#index", as: :backup
+  get "backup/export", to: "backups#export", as: :backup_export
+  post "backup/import", to: "backups#import", as: :backup_import
+  get "backup/download_daily", to: "backups#download_daily", as: :backup_download_daily
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
